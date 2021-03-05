bed1c2cbf28502e543c3a9acd3ac53a1ae5e1edd
Mia Burger <mia.burger@gmx.de>
2021-03-05 23:28:28 +0100

    Heatplot finished

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,5 +1,7 @@
 var width = 800;
 var height = 400;
+var colors = ["#333dff", "#33caff", "#33fffe", "#FFfa00", "#FFbe00", "#FF8c00", "#FF5a00", "#FF1e00", "#af0000"];
+var tempScale = [];
 
 var tooltip = d3
   .select("#visHolder")
@@ -11,14 +13,7 @@ var svgContainer = d3
   .select("#visHolder")
   .append("svg")
   .attr("width", width + 100)
-  .attr("height", height + 60);
-
-svgContainer
-  .append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("x", -350)
-  .attr("y", 20)
-  .text("months");
+  .attr("height", height + 100);
 
 // read in the data
 d3.json(
@@ -27,34 +22,30 @@ d3.json(
 
   console.log(data);
   var globalData = new Array(12);
-  console.log(globalData);
 
-  data.monthlyVariance.forEach( (el) => {
+  var monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  data.monthlyVariance.forEach((el) => {
     let i = el.month;
-    if (!globalData[i-1]) (globalData[i-1] = []);
-    globalData[i-1].push({ year: el.year, temp: 8.66 + el.variance });
+    if (!globalData[i - 1]) (globalData[i - 1] = []);
+    globalData[i - 1].push({ year: el.year, temp: 8.66 + el.variance });
   });
 
-  console.log(globalData);
+  var temp = data.monthlyVariance.map(el => (el.variance + 8.66));
+  var tempMin = d3.min(temp);
+  var tempMax = d3.max(temp);
+  var diff = (tempMax - tempMin) / colors.length;
+  tempscale = colors.map( (el, i) => ( tempMin + i*diff));
+  console.log(tempscale);
 
-  /*
-  const xscale = d3
-    .scaleLinear()
-    .domain([d3.min(year) - 1, d3.max(year) + 1])
-    .range([0, width]);
-  var x_axis = d3.axisBottom(xscale).tickFormat(d3.format("d"));
+  var len = globalData[0].length;
 
-  var yscale = d3
-    .scaleTime()
-    .domain([d3.min(time), d3.max(time)])
-    .range([0, height]);
-  var y_axis = d3.axisLeft(yscale).tickFormat(d3.timeFormat("%M:%S"));
+  const yScale = d3
+    .scaleBand()
+    .domain(monthArr)
+    .range([height, 0]);
 
-  svgContainer
-    .append("g")
-    .call(x_axis)
-    .attr("id", "x-axis")
-    .attr("transform", "translate(80, 420)");
+  const y_axis = d3.axisLeft(yScale);
 
   svgContainer
     .append("g")
@@ -62,64 +53,84 @@ d3.json(
     .attr("id", "y-axis")
     .attr("transform", "translate(80, 20)");
 
+
+  const xscale = d3
+    .scaleLinear()
+    .domain([globalData[0][0].year, globalData[0][len - 1].year + 1])
+    .range([0, width]);
+  var x_axis = d3.axisBottom(xscale).tickFormat(d3.format("d"));
+
+  svgContainer
+    .append("g")
+    .call(x_axis)
+    .attr("id", "x-axis")
+    .attr("transform", "translate(80, 420)");
+
+
+  var cellHeight = height / 12;
+  var cellWidth = width / (globalData[0][len - 1].year - globalData[0][0].year);
+
   d3.select("svg")
-    .selectAll("circle")
-    .data(data)
+    .selectAll("rect")
+    .data(data.monthlyVariance)
     .enter()
-    .append("circle")
-    .attr("class", "dot")
-    .attr("data-xvalue", (d) => d.Year)
-    .attr("data-yvalue", (d) => d.Time)
-    .attr("cx", (d) => xscale(d.Year))
-    .attr("cy", (d) => yscale(d.Time))
-    .attr("r", 10)
-    .attr("fill", (d) => d.Color)
+    .append("rect")
+    .attr("class", "cell")
+    .attr("data-month", (d, i) => d.month - 1)
+    .attr("data-year", (d) => d.year)
+    .attr("data-temp", (d) => 8.66 + d.variance)
+    .attr("x", (d) => xscale(d.year))
+    .attr("y", (d) => height - cellHeight * d.month)
+    .attr("width", cellWidth)
+    .attr("height", cellHeight)
+    .attr("fill", (d) => getColor(d.variance + 8.66))
     .attr("transform", "translate(80, 20)")
     .on("mouseover", function (event, d) {
       tooltip.transition().duration(200).style("opacity", 0.9);
       tooltip
-        .attr("data-year", d.Year)
-        .attr("data-time", d.Time)
-        .html(d.Name + "<br>" + d.Year + "<br>" + d.Time0)
+        .attr("data-year", d.year)
+        .attr("data-temp", d.variance + 8.66)
+        .attr("data-month", d.month - 1)
+        .html(d.year + ", " + monthArr[d.month] + ": <br>"
+          + (d.variance + 8.66).toFixed(2) + " °C, &Delta;"
+          + (d.variance).toFixed(2) + " °C")
         .style("left", event.layerX + 30 + "px")
         .style("top", event.layerY + 30 + "px");
-      d3.select(this).style("fill", "lightblue");
     })
 
     .on("mouseout", function (d) {
       tooltip.transition().duration(500).style("opacity", 0);
-      d3.select(this).style("fill", (d) => d.Color);
     });
 
   var legendContainer = d3.select("svg")
     .append("g")
     .attr("id", "legend")
-    .attr("border", "1px solid grey");
 
   var legend = legendContainer
     .selectAll("#legend")
-    .data(["orange", "green"])
+    .data(colors)
     .enter()
     .append("g")
     .attr("class", "legend-label")
-    .attr("transform", function (d, i) {
-      return "translate(0," + (height / 2 - i * 20) + ")";
-    });
   legend
     .append("rect")
-    .attr("x", width - 18)
-    .attr("width", 18)
-    .attr("height", 18)
+    .attr("x", (d, i) => 100 + 45*i)
+    .attr("y", height + 50 )
+    .attr("width", 45 )
+    .attr("height", 10 )
     .style("fill", (d) => d);
   legend
     .append("text")
-    .attr("x", width - 24)
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "end")
-    .text((d) => {
-      if (d == "orange") return "Riders with doping allegations";
-      else return "Riders with no doping allegations";
-    });
-    */
+    .attr("x", (d, i) => 100 + 45*i )
+    .attr("y", height + 70 )
+    .attr("font-size", "10px")
+    .text((d, i) => (tempscale[i]).toFixed(1) + "°C");
+
+  function getColor(temp) {
+    var colorIdx = Math.floor((temp - tempMin) / diff);
+    if (colorIdx > 8) colorIdx = 8;
+    return (colors[colorIdx]);
+  }
+
+
 });
